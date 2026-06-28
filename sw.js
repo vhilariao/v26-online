7e3cc7fd127573e52d9262918b65886e45b6ccb3
vhilariao <vhilariao@gmail.com>
2026-06-28 14:50:59 -0300

    Atualiza frontend funcional v26-web-v1.1

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'v26-web-v1';
+const CACHE = 'v26-web-v1-1';
 const ASSETS = ['./index.html', './manifest.webmanifest', './icon.svg'];
 self.addEventListener('install', (event) => {
   event.waitUntil(caches.open(CACHE).then(cache => cache.addAll(ASSETS)));
@@ -9,6 +9,6 @@ self.addEventListener('activate', (event) => {
 self.addEventListener('fetch', (event) => {
   const url = new URL(event.request.url);
   if (url.origin === self.location.origin) {
-    event.respondWith(caches.match(event.request).then(res => res || fetch(event.request)));
+    event.respondWith(fetch(event.request).catch(() => caches.match(event.request)));
   }
 });
